94a0233b1a0a5429232fb59fff2104a3131911a6
Jeffrey Liu <jeffkliu@gmail.com>
2023-03-09 12:52:43 -0600

    Created AccordionPage.js
    Refactored App.js to create new component - dropdown

--- src/App.js
+++ src/App.js
@@ -1,26 +1,5 @@
-import Accordion from "./components/Accordion";
 function App() {
 
-    const items = [
-        {
-            id: 'abc231',
-            label: 'Can I use React on a project?',
-            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-        },
-        {
-            id: 'c3232sdf',
-            label: 'Can I use Javascript on a project?',
-            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-
-        },
-        {
-            id: 'cdsdsdsds',
-            label: 'Can I use CSS on a project?',
-            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-        },
-    ];
-
-    return <Accordion items={items} />
 }
 
 export default App;
