27296ade5987d81fd6aca8bdeb013c04c56b7b1c
Jeffrey Liu <jeffkliu@gmail.com>
2023-03-17 16:27:44 -0500

    added handling of CMD and CTRL keys

--- src/components/Link.js
+++ src/components/Link.js
@@ -7,11 +7,17 @@ function Link({ to, children }) {
     const { navigate } = useContext(NavigationContext);
 
     const handleClick = (event) => {
+        console.log(event);
+
+
+    if(event.metaKey || event.metaKey){
+        return;
+    }
         event.preventDefault();
         navigate(to);
     };
 
-    return <a onClick={handleClick}>{children}</a>
+    return <a href={to} onClick={handleClick}>{children}</a>
 }
 
 export default Link;
