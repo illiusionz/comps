44c9f0fc393f7ad086a7385401a5ed99550e0386
Jeffrey Liu <jeffkliu@gmail.com>
2023-03-17 16:21:40 -0500

    created Route.js component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import Link from "./components/Link";
+import Route from "./components/Route";
+import AccordionPage from './pages/AccordionPage';
+import DropdownPage from './pages/DropdownPage';
+import ButtonPage from './pages/ButtonPage';
+
 
 function App() {
     return (
         <div>
             <Link to="/accordion">Go to accordion</Link>
             <Link to="/dropdown">Go to dropdown</Link>
+        
+
+            <div>
+                <Route path="/accordion">
+                    <AccordionPage />
+                </Route>
+                <Route path="/dropdown">
+                    <DropdownPage />
+                </Route>
+
+            </div>
         </div>
 
     );
