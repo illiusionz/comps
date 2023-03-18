c154663346bfe767d58c892e4f28e817b9bfdfb7
Jeffrey Liu <jeffkliu@gmail.com>
2023-03-18 16:12:43 -0500

    Added Styles

--- src/components/SortableTable.js
+++ src/components/SortableTable.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { GoArrowSmallDown, GoArrowSmallUp } from "react-icons/go";
 import Table from "./Table";
 
 function SortableTable(props){
@@ -28,9 +29,16 @@ function SortableTable(props){
 
         return {
             ...column,
-            header: () => <th onClick={ () => handleClick(column.label) }>{column.label} IS SORTABLE</th>
-        }
-
+            header: () => (
+                <th className="cursor-pointer hover:bg-gray-100" onClick={ () => handleClick(column.label) }>
+                    <div className="flex items-center">
+                        {getIcons(column.label, sortBy, sortOrder)}
+                        {column.label} 
+                    </div>
+                
+                </th>
+            ),
+        };
     });
 
     //Only sory data if sortOrder and sortBy are not null
@@ -61,6 +69,30 @@ function SortableTable(props){
             <Table {...props} data={sortedData} config= {updatedConfig} />
         </div>
     );
+}
+
+
+function getIcons(label, sortBy, sortOrder){
+    if(label !== sortBy){
+        return (
+            <div>
+                <GoArrowSmallUp />
+                <GoArrowSmallDown />
+            </div>
+        );
+    }
+    if(sortOrder === null){
+        return (
+            <div>
+                <GoArrowSmallUp />
+                <GoArrowSmallDown />
+            </div>
+        );
+    } else if(sortOrder === 'asc'){
+        return <div><GoArrowSmallUp /></div>
+    }else if(sortOrder === 'desc'){
+        return <div><GoArrowSmallDown /></div>
+    }
 }
 
 export default SortableTable;
